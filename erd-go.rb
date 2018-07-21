e68784d686c54e6560b67e6efb22bba064b950d1
Koki Oyatsu <kaishuu0123@gmail.com>
2018-07-21 19:13:37 +0900

    fix head build

--- erd-go.rb
+++ erd-go.rb
@@ -8,7 +8,7 @@ class ErdGo < Formula
   head 'https://github.com/kaishuu0123/erd-go.git', :branch => 'master'
 
   if build.head?
-    depends_on 'go@1.9' => :build
+    depends_on 'go' => :build
     depends_on 'make' => :build
   end
 
@@ -18,6 +18,8 @@ class ErdGo < Formula
       system 'go', 'get', 'github.com/Masterminds/glide'
       system 'go', 'get', 'github.com/jteeuwen/go-bindata'
       system 'go', 'get', 'github.com/pointlander/peg'
+      mkdir_p buildpath/'src/github.com/kaishuu0123'
+      ln_s buildpath, buildpath/'src/github.com/kaishuu0123/erd-go'
       system 'make'
     else
       system 'mv', 'darwin_amd64_erd-go', 'erd-go'
