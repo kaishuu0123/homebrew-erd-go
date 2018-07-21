8e1f3129d4c558f609de9535ccddd4caa43a2ca8
Koki Oyatsu <kaishuu0123@gmail.com>
2018-07-21 18:59:14 +0900

    non-build install

--- erd-go.rb
+++ erd-go.rb
@@ -3,16 +3,20 @@ require 'formula'
 HOMEBREW_ERD_GO_VERSION='1.4.0'
 class ErdGo < Formula
   homepage 'https://github.com/kaishuu0123/erd-go'
-  url "https://github.com/kaishuu0123/erd-go/archive/v#{HOMEBREW_ERD_GO_VERSION}.zip"
+  url "https://github.com/kaishuu0123/erd-go/releases/download/v#{HOMEBREW_ERD_GO_VERSION}/darwin_amd64_erd-go"
   version HOMEBREW_ERD_GO_VERSION
   head 'https://github.com/kaishuu0123/erd-go.git', :branch => 'master'
 
-  depends_on 'go@1.9' => :build
-  depends_on 'make' => :build
+  if build.head?
+    depends_on 'go@1.9' => :build
+    depends_on 'make' => :build
+  end
 
   def install
-    ENV['GOPATH'] = buildpath
-    system 'make'
+    if build.head?
+      ENV['GOPATH'] = buildpath
+      system 'make'
+    end
     bin.install 'erd-go'
   end
 end
