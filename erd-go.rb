15a2302194c1217ffb57c077a1bdf478dc1fe411
Koki Oyatsu <kaishuu0123@gmail.com>
2020-03-17 11:02:51 +0900

    update erd-go v1.4.5

--- erd-go.rb
+++ erd-go.rb
@@ -1,17 +1,16 @@
-HOMEBREW_ERD_GO_VERSION="1.4.3".freeze
+HOMEBREW_ERD_GO_VERSION="1.4.5".freeze
 class ErdGo < Formula
   desc "Translates plain text of RDB schema to entity-relationship diagram"
   homepage "https://github.com/kaishuu0123/erd-go"
   url "https://github.com/kaishuu0123/erd-go/releases/download/v#{HOMEBREW_ERD_GO_VERSION}/darwin_amd64_erd-go"
 
   version HOMEBREW_ERD_GO_VERSION
-  sha256 "f974343e19f2022d08b080c933d4f38c2cd0e1204e9c4489b07023b77eb2369e"
+  sha256 "c260cf3d05952d3d07a7d76d985c26c5780e5b3ec58f1603c46f70f2c320e07d"
 
   head "https://github.com/kaishuu0123/erd-go.git", :branch => "master"
 
   if build.head?
     depends_on "go" => :build
-    depends_on "glide" => :build
     depends_on "go-bindata" => :build
     depends_on "make" => :build
   end
@@ -19,7 +18,6 @@ class ErdGo < Formula
   def install
     if build.head?
       ENV["GOPATH"] = buildpath
-      ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
       ENV.prepend_create_path "PATH", buildpath/"bin"
       dir = buildpath/"src/github.com/kaishuu0123/erd-go"
       dir.install buildpath.children - [buildpath/".brew_home"]
